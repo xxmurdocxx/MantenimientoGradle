a4b908f3a786ebee19c0c6ce86d80e3d3fc1ad69
Alberto Quineche <albertoquineche86@gmail.com>
2018-11-18 21:44:06 -0500

    Update Web

--- ProyectoMantenimiento/src/test/java/LibraryTest.java
+++ ProyectoMantenimiento/src/test/java/LibraryTest.java
@@ -6,11 +6,9 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
-		//
-        //Library classUnderTest = new Library();
-        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-		
+	    
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());	
 		System.out.print("Hola");
-	    //hola!"!!
     }
 }
