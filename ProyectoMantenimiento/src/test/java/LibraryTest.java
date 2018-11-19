6cd9562c451d586b4f674f67309a725aa6a1f2af
Aquineche <albertoquineche86@gmail.com>
2018-11-18 21:52:11 -0500

    cambios aq

--- ProyectoMantenimiento/src/test/java/LibraryTest.java
+++ ProyectoMantenimiento/src/test/java/LibraryTest.java
@@ -10,5 +10,6 @@ public class LibraryTest {
 	    Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());	
 		System.out.print("Hola");
+		
     }
 }
