027ec25044532a00cb8df10025561a2069fe164f
Aquineche <albertoquineche86@gmail.com>
2018-11-18 22:13:33 -0500

    cambios aq

--- ProyectoMantenimiento/src/test/java/LibraryTest.java
+++ ProyectoMantenimiento/src/test/java/LibraryTest.java
@@ -7,8 +7,9 @@ import static org.junit.Assert.*;
 public class LibraryTest {
 
     @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
+		Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        System.out.print("hola mundo");
 		
     }
 }
