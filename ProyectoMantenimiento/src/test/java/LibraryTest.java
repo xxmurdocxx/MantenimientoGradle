b3cc3a7817e076a74a7143ba9279b50b197b7596
Alberto Quineche <albertoquineche86@gmail.com>
2018-11-18 21:45:10 -0500

    Update LibraryTest.java

--- ProyectoMantenimiento/src/test/java/LibraryTest.java
+++ ProyectoMantenimiento/src/test/java/LibraryTest.java
@@ -9,6 +9,5 @@ public class LibraryTest {
 	    
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());	
-		System.out.print("Hola");
     }
 }
